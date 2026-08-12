07cc97199abfd1c098d02ee1cf515dab039856cd
Clark Sean Payabyab <seanpayabyab17@gmail.com>
2026-08-12 22:51:39 +0800

    feat: add LightRays component to background

--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,26 @@
+import LightRays from "@/components/LightRays"
 import Hero from "@/components/sections/hero"
 import { Button } from "@/components/ui/button"
 
 export default function Page() {
   return (
-    <div className="min-h-screen w-full flex justify-center">
-      <main className="w-full max-w-2xl min-w-0 flex flex-col gap-4 text-sm leading-loose mx-auto p-4 sm:p-6 ">
+    <div className="relative flex min-h-screen w-full justify-center">
+      <LightRays
+        raysOrigin="top-center"
+        raysColor="#ffffff"
+        raysSpeed={1}
+        lightSpread={1}
+        rayLength={2}
+        followMouse={true}
+        mouseInfluence={0.1}
+        noiseAmount={0}
+        distortion={0}
+        pulsating={false}
+        fadeDistance={0.5}
+        saturation={1}
+      />
+      <main className="relative z-10 mx-auto flex w-full max-w-2xl min-w-0 flex-col gap-4 p-4 text-sm leading-loose sm:p-6">
         <Hero />
-        <div>
-          <h1 className="font-medium">Project ready!</h1>
-          <p>You may now add components and start building.</p>
-          <p>We&apos;ve already added the button component for you.</p>
-          <Button className="mt-2">Button</Button>
-        </div>
-        <div className="font-mono text-xs text-muted-foreground">
-          (Press <kbd>d</kbd> to toggle dark mode)
-        </div>
       </main>
     </div>
   )
